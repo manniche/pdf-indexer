19a117136b643884c616801f85e00e6007f6473c
Steen Manniche <steen@manniche.net>
2012-05-06 21:57:20 +0200

    using LucenePDFDocument for storing the pdf extration in, instead of plain text

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject pdf-indexer "1.0.0-SNAPSHOT"
   :description "command line program that indexes a PDF file to a global index"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.pdfbox/pdfbox "1.6.0"]]
+                 [org.apache.pdfbox/pdfbox "1.6.0"]
+                 [org.apache.pdfbox/pdfbox-lucene "1.6.0"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]]
   :main pdf-indexer.core
   )
--- src/pdf_indexer/core.clj
+++ src/pdf_indexer/core.clj
@@ -1,15 +1,24 @@
 (ns pdf-indexer.core)
 
-(use 'clojure.string)
+(use '[clojure.string :only (split)])
 (import org.apache.pdfbox.pdmodel.PDDocument)
 (import org.apache.pdfbox.util.PDFTextStripper)
+(import org.apache.pdfbox.lucene.LucenePDFDocument)
+
 
 (defn extract-text [pdf]
   (let [stripper (PDFTextStripper.)])
   (split (.getText (PDFTextStripper.) (. PDDocument load pdf) ) #"\r\n")
-  )  
-  
+  )
+
+(defn extract-document [pdf]
+  (let [lucenedoc (LucenePDFDocument.)]
+    (let [stripper (PDFTextStripper.)]
+      (let [f (clojure.java.io/file pdf)]
+      (.setTextStripper lucenedoc stripper)
+      (.convertDocument lucenedoc f)
+  ))))
 
 (defn -main [filepath]
-  (extract-text filepath)
+  (extract-document filepath)
   )
