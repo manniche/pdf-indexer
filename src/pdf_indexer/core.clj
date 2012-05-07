7bfb5da8383b28dc81b7ff9efb9428e3d625af9c
Steen Manniche <steen@manniche.net>
2012-05-07 13:24:34 +0200

    adding lucene document introspection

--- src/pdf_indexer/core.clj
+++ src/pdf_indexer/core.clj
@@ -1,6 +1,7 @@
 (ns pdf-indexer.core)
 
 (use '[clojure.string :only (split)])
+
 (import org.apache.pdfbox.pdmodel.PDDocument)
 (import org.apache.pdfbox.util.PDFTextStripper)
 (import org.apache.pdfbox.lucene.LucenePDFDocument)
@@ -15,10 +16,20 @@
   (let [lucenedoc (LucenePDFDocument.)]
     (let [stripper (PDFTextStripper.)]
       (let [f (clojure.java.io/file pdf)]
-      (.setTextStripper lucenedoc stripper)
-      (.convertDocument lucenedoc f)
-  ))))
+        (.setTextStripper lucenedoc stripper)
+        (.convertDocument lucenedoc f)
+        ))))
+
+(defn inspect-lucene-doc
+  "inspect a lucene document by field"
+  [lucene-doc]
+  (let [fields (.getFields lucene-doc)]
+    (doseq [field fields]
+      (println (.name field) (.stringValue field))
+      )))
+
 
 (defn -main [filepath]
-  (extract-document filepath)
+  (inspect-lucene-doc (extract-document filepath))
+  ;;  (take 10 (extract-text filepath))
   )
